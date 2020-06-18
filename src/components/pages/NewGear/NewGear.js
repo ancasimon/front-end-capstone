765268796ad8ee5627e17802d300224047b175c7
Anca Simon <ancarsimon@gmail.com>
2020-06-18 13:31:31 -0500

    able to navigate to each navbar menu item and pages for adding, editing, and viewing gear

---
+++ src/components/pages/NewGear/NewGear.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './NewGear.scss';
+
+class NewGear extends React.Component {
+  render() {
+    return (
+      <div className="NewGear">
+        <h1>Add a New Gear Item Page</h1>
+      </div>
+    );
+  }
+}
+
+export default NewGear;
