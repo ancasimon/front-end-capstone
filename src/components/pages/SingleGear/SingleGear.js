ed13114dfbe41c06bd8f3639ab441c7f49ae852d
Anca Simon <ancarsimon@gmail.com>
2020-06-20 18:50:07 -0500

    finalized single gear view page

--- src/components/pages/SingleGear/SingleGear.js
+++ src/components/pages/SingleGear/SingleGear.js
@@ -47,68 +47,77 @@ class SingleGear extends React.Component {
 
     return (
       <div className="SingleGear col-12">
-        <h1>Single Gear Details Page</h1>
+        <h1>Details about your <span className="caps">{gearItem.item}</span>: {gearItem.brand} - {gearItem.model}</h1>
 
-        <Link to='/gear' className="btn-lg btn-danger"><i class="far fa-window-close"></i></Link>
+        <Link to='/gear' className="btn-lg btn-danger"><i className="far fa-window-close"></i></Link>
 
-        <h2>{gearItem.item}</h2>
-        <img className="gearPhoto" src={gearItem.imageUrl} alt={gearItem.item} />
+        <div className="container col-12">
+          <div className="row">
+            <div className="col-sm">
+              <img className="gearSinglePhoto" src={gearItem.imageUrl} alt={gearItem.item} />
+            </div>
+            <div className="col-sm">
+              <div className="row">
+                <h6 className="col-sm align-middle">Function: </h6>
+                <p className="col-sm align-middle"><img className="gearIcon" src={gearFunction.imageUrl} alt={gearFunction.name} />   {gearFunction.name}</p>
+              </div>
 
-        <div className="row justify-content-center col-12">
+              <div className="row">
+                <h6 className="col-sm align-middle">Seasons: </h6>
+                {
+                  gearSeasons
+                    ? <div className="col-sm align-middle">{gearSeasons.map((item) => <div className="row justify-content-center" key={item.id}><img className="gearIcon" src={item.imageUrl} alt={item.name} />{item.name}</div>)}</div>
+                    : <p className="col-sm align-middle">N/A</p>
+                }
+              </div>
+
+              <div className="row">
+                <h6 className="col-sm align-middle">Weather: </h6>
+                <p className="col-sm align-middle"><img className="gearIcon" src={gearWeather.imageUrl} alt={gearWeather.name} />   {gearWeather.name}</p>
+              </div>
+
+              <div className="row">
+                <h6 className="col-sm align-middle">Parties: </h6>
+                {
+                  gearParties
+                    ? <div className="col-sm align-middle">{gearParties.map((item) => <div className="row justify-content-center" key={item.id}><img className="gearIcon" src={item.imageUrl} alt={item.name} />   {item.name}</div>)}</div>
+                    : <p className="col-sm align-middle">N/A</p>
+                }
+              </div>
+
+              <div className="row">
+                <h6 className="col-sm align-middle">Weight: </h6>
+                <h6 className="col-sm align-middle">{gearItem.weightInGrams * 1 } grams</h6>
+              </div>
+
+              <div className="row">
+                <h6 className="col-sm align-middle">Can you use it only at established campsites? </h6>{ gearItem.forEstablishedCampsite ? <p className="col-sm align-middle">Indeed!</p> : <p className="col-sm align-middle">Nope, can use it anytime anywhere!</p> }
+              </div>
+
+              <div className="row">
+                <h6 className="col-sm align-middle">Available? </h6>{ gearItem.isAvailable ? <p className="col-sm align-middle">Ready to go!</p> : <p className="col-sm align-middle">Sorry, you killed it a long time ago!</p> }
+              </div>
+
+              <div className="row">
+                <h6 className="col-sm align-middle">When it was made: </h6><p className="col-sm align-middle"> Year {gearItem.manufactureYear}</p>
+              </div>
+
+              <div className="row">
+                <h6 className="col-sm align-middle">When you may have to replace it: </h6><p className="col-sm align-middle"> Year {gearItem.expirationYear}</p>
+              </div>
+
+            </div>
+          </div>
+
+        {/* <div className="row justify-content-center col-12">
           <h4>{gearItem.brand}</h4> - <h4>{gearItem.model}</h4>
+        </div> */}
+
+          <div className="row justify-content-center">
+            <Link to={editLink} className="col-sm-4 btn btn-lg p-1"><i className="fas fa-pencil-alt"></i></Link>
+            <button className="col-sm-4 btn-lg pointerHand p-1"><i className="fas fa-trash-alt"></i></button>
+          </div>
         </div>
-
-        <div className="row justify-content-center col-12">
-          <h4>Function: </h4>
-          <h4><img className="gearIcon" src={gearFunction.imageUrl} alt={gearFunction.name} /> - {gearFunction.name}</h4>
-        </div>
-
-        <div className="row justify-content-center col-12">
-          <h4>Seasons: </h4>
-          {
-            gearSeasons
-              ? <h4>{gearSeasons.map((item) => <div className="row col-12"><img className="gearIcon" key={item.id} src={item.imageUrl} alt={item.name} />{item.name}</div>)}</h4>
-              : <h4>N/A</h4>
-          }
-        </div>
-
-        <div className="row justify-content-center col-12">
-          <h4>Weather: </h4>
-          <h4><img className="gearIcon" src={gearWeather.imageUrl} alt={gearWeather.name} />{gearWeather.name}</h4>
-        </div>
-
-        <div className="row justify-content-center col-12">
-          <h4>Parties: </h4>
-          {
-            gearParties
-              ? <div>{gearParties.map((item) => <div className="row"><img className="gearIcon" key={item.id} src={item.imageUrl} alt={item.name} />{item.name}</div>)}</div>
-              : <h4>N/A</h4>
-          }
-        </div>
-
-        <h4>Weight: {gearItem.weightInGrams * 1 } grams</h4>
-
-        <div className="row col-12">
-          <h5>Can you use it only at established campsites? </h5>{ gearItem.forEstablishedCampsite ? <h5>Indeed!</h5> : <h5>Nope, can use it anytime anywhere!</h5> }
-        </div>
-
-        <div className="row col-12">
-          <h5>Available? </h5>{ gearItem.isAvailable ? <h5>Ready to go!</h5> : <h5>Sorry, you killed it a long time ago!</h5> }
-        </div>
-
-        <div className="row col-12">
-          <h5>When it was made: </h5><h5> Year {gearItem.manufactureYear}</h5>
-        </div>
-
-        <div className="row col-12">
-          <h5>When you may have to replace it: </h5><h5> Year {gearItem.expirationYear}</h5>
-        </div>
-
-        <div className="row col-12 justify-content-center">
-          <Link to={editLink} className="btn btn-lg p-1"><i className="fas fa-pencil-alt"></i></Link>
-          <button className="btn-lg pointerHand p-1"><i className="fas fa-trash-alt"></i></button>
-        </div>
-
       </div>
     );
   }
