89b2cbd0689c3430b1b8e61fe7c0c84313aa8825
Anca Simon <ancarsimon@gmail.com>
2020-07-06 01:02:25 -0500

    displaying checkboxes for all gear on edit trip page

--- src/components/pages/EditTrip/EditTrip.js
+++ src/components/pages/EditTrip/EditTrip.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Swal from 'sweetalert2';
 import { Link } from 'react-router-dom';
+import { Table } from 'reactstrap';
 
 import authData from '../../../helpers/data/authData';
 import tripsData from '../../../helpers/data/tripsData';
@@ -24,6 +25,7 @@ class EditTrip extends React.Component {
     tripImageUrl: '',
     tripEstablishedCampsite: false,
     tripDestination: '',
+    tripGear: [],
   };
 
   componentDidMount() {
@@ -65,6 +67,7 @@ class EditTrip extends React.Component {
           tripImageUrl: currentTrip.imageUrl,
           tripEstablishedCampsite: currentTrip.isEstablishedCampsite,
           tripDestination: currentTrip.destination,
+          tripGear: currentTrip.allGearWithChecks,
         });
       })
       .catch((err) => console.error('could not update trip info', err));
@@ -163,6 +166,7 @@ class EditTrip extends React.Component {
       tripImageUrl,
       tripEstablishedCampsite,
       tripDestination,
+      tripGear,
     } = this.state;
 
     const buildWeatherList = () => weatherList.map((weatherValue) => (
@@ -175,6 +179,27 @@ class EditTrip extends React.Component {
 
     const buildPartyList = () => partyList.map((partyValue) => (
       <option key={partyValue.id} value={partyValue.id}>{partyValue.name}</option>
+    ));
+
+    const buildGearList = () => tripGear.map((gearObject) => (
+      <tbody key={gearObject.id}>
+        <tr>
+          <td class="form-check">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              value=""
+              id="defaultCheck1"
+            />
+            <label class="form-check-label" for="defaultCheck1">
+              {gearObject.item}
+              </label>
+          </td>
+          <td className="d-none d-md-table-cell"><img className="gearPhoto photoBorder" src={gearObject.imageUrl} alt={gearObject.item} /></td>
+          <td>{gearObject.brand}</td>
+          <td className="d-none d-md-table-cell">{gearObject.model}</td>
+        </tr>
+      </tbody>
     ));
 
     return (
@@ -290,6 +315,28 @@ class EditTrip extends React.Component {
               </select>
             </div>
 
+          </div>
+
+          <div>
+            <Table hover className="inputBorder">
+              <thead>
+                <tr>
+                  <th>Item</th>
+                  <th className="d-none d-md-table-cell">Image</th>
+                  <th>Brand</th>
+                  <th className="d-none d-md-table-cell">Model</th>
+                  {/* <th className="d-none d-sm-table-cell">Function</th>
+                  <th className="d-none d-sm-table-cell">Seasons</th>
+                  <th className="d-none d-sm-table-cell">Weather</th>
+                  <th className="d-none d-sm-table-cell">Party</th>
+                  {/* <th>Weight (gr.)</th> */}
+                  {/* <th className="d-none d-md-table-cell">Available?</th> */}
+                  {/* <th>Exp. Yr.</th> */}
+                  {/* <th>Actions</th> */}
+                </tr>
+              </thead>
+              {buildGearList()}
+            </Table>
           </div>
 
           <button type="submit" className="btn greenButtons" onClick={this.updateTrip}>Save Your Changes</button>
